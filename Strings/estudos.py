8022bf736634509a034bdafeae4f83bd1cc0a2ea
Pedro Martins de Oliveira <142221456+OliveiraPedro09@users.noreply.github.com>
2023-10-26 21:55:13 -0300

    Estudo Python Strings

---
+++ Strings/estudos.py
@@ -0,0 +1,11 @@
+#Exemplos
+
+frase = "Ronaldo é o melhor atacante"  #23 espaços
+
+print(frase[9:21:2]) # O 9:21 ele pega os caractéres do espaço 9 até o 21 o :2 é acrescentado para ele pular de 2 em 2.
+
+print(len(frase)) # Mostra o tamanho de uma string.
+
+frase.count('o') # serve para mostrar quantas vezes tem um determinado caractére em uma string. 
+
+
